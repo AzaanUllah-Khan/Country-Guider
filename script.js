88ef40d392aebef68b22713a1468c0266a81c93a
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2023-08-07 17:26:26 +0500

    api addded styling updated

---
+++ script.js
@@ -0,0 +1,35 @@
+// API: https://restcountries.com/v3.1/name/country?fullText=true
+var container = document.getElementById("show")
+function fetchAPi() {
+    var country = document.getElementById("input").value
+    var info = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
+    fetch(info)
+        .then((response) => response.json())
+        .then((data) => {
+            var data = data[0]
+            const countryFlag = data.flags.svg;
+            const countryName = data.name.common;
+            const countryOfficialName = data.name.official;
+            const countryCapital = data.capital ? Object.values(data.capital) : "Has No Capital";
+            const countryTotalArea = data.area;
+            const countryTotalPopulation = data.population;
+            const countryLanguage = Object.values(data.languages);
+            const countryRegion = data.region;
+            const countryMap = data.maps.googleMaps;
+            const countryCurrencies = Object.keys(data.currencies);
+            const countryTimeZone = Object.values(data.timezones);
+            console.log(data);
+
+            document.getElementById("flag").src = countryFlag
+            document.getElementById("name").innerHTML = countryName
+            document.getElementById("official").innerHTML = countryOfficialName
+            document.getElementById("capital").innerHTML = countryCapital
+            document.getElementById("total").innerHTML = countryTotalArea
+            document.getElementById("population").innerHTML = countryTotalPopulation
+            document.getElementById("lang").innerHTML = countryLanguage
+            document.getElementById("region").innerHTML = countryRegion
+            document.getElementById("map").href = countryMap
+            document.getElementById("currency").innerHTML = countryCurrencies
+            document.getElementById("timeZone").innerHTML = countryTimeZone
+        })
+}
